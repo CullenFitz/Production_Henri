34cd98d6b4faea45fa99af717f452fc8459782fe
CullenFitz <61251147+CullenFitz@users.noreply.github.com>
2021-10-12 08:23:18 -0400

    Update HealthStats.py

--- HealthStats.py
+++ HealthStats.py
@@ -1,1 +1,13 @@
 #Sends health stats to user at specified time intervals 
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
+
+driver.get("https://connect.garmin.com/signin")
+
+sleep_total_time = driver.find_element_by_class_name('sleep-total-time')
+
+print(sleep_total_time)
